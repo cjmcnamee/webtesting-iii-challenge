54aa12333f52aa6f157a4499d39a1d7298db6612
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-08-19 19:14:15 -0400

    aaaaaaaaaaaaaaaaa

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { render, fireEvent } from '@testing-library/react';
 
-import Display from './Display';
+import Display, { closedClass } from './Display';
 
 describe('<Display />', () => {
   it('matches snapshot', () => {
@@ -10,13 +10,19 @@ describe('<Display />', () => {
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
-  it('displays open by default', () => {
+  it('displays "open" by default', () => {
     const { queryByText } = render(<Display />);
     expect(queryByText(/Open/i)).toBeTruthy();
   })
 
-  it('displays unlocked by default', () => {
+  it('displays "unlocked" by default', () => {
     const { queryByText } = render(<Display />);
     expect(queryByText(/Unlocked/i)).toBeTruthy();
   })
+  // it('uses "red-led" class when locked or closed', () => {
+  //   const { getByTestId, queryByText } = render(<Display />);
+  //   const expected = 'led red-led';
+  //   expect(queryByText(/Locked/i)).toBeTruthy();
+  //   expect(closedClass).toBe('red-led');
+  // })
 });
